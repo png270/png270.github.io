a207aaef8b1f1209e21728d7f1bf6fcac7c3ac67
png270 <tobaccoiscancer@gmail.com>
2026-06-28 16:39:46 -0400

    link fixes & project updates

--- .astro/content-assets.mjs
+++
@@ -1,5 +0,0 @@
-
-import __ASTRO_IMAGE_IMPORT_Zs4Ge3 from "./PixelatedGreenTreeSide.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fshowing-off-blog-features%2Findex.md";
-import __ASTRO_IMAGE_IMPORT_1W6MYL from "./cover.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fshowing-off-blog-features%2Findex.md";
-export default new Map([["./PixelatedGreenTreeSide.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fshowing-off-blog-features%2Findex.md", __ASTRO_IMAGE_IMPORT_Zs4Ge3], ["./cover.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fshowing-off-blog-features%2Findex.md", __ASTRO_IMAGE_IMPORT_1W6MYL]]);
-		
